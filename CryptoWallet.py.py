730fbdaec85ff54cfa27b5d359df18c6eb1f53c5
mishranil223 <142275347+mishranil223@users.noreply.github.com>
2023-08-25 10:46:09 -0400

    Add files via upload
    A simple Crypto Wallet for sending and receiving funds.

---
+++ CryptoWallet.py.py
@@ -0,0 +1,56 @@
+# Define a class called CryptoWallet to represent a cryptocurrency wallet.
+class CryptoWallet:
+    # Initialize the wallet with a starting balance.
+    def __init__(self, balance):
+        self.balance = balance
+
+    # Method to send cryptocurrency with a fee.
+    def send(self, amount):
+        fee = amount * 0.01  # Calculate the transaction fee.
+        total_amount = amount + fee  # Calculate the total transaction amount.
+
+        # Check if the total transaction amount is greater than the wallet balance.
+        if total_amount > self.balance:
+            print("Not enough balance.")
+        else:
+            self.balance -= total_amount  # Deduct the total transaction amount from the balance.
+            print(f"Sent {amount:.2f} crypto with a fee. New balance: {self.balance:.2f}")
+
+    # Method to receive cryptocurrency and update the balance.
+    def receive(self, amount):
+        self.balance += amount  # Add the received amount to the balance.
+        print(f"Received {amount:.2f} crypto. New balance: {self.balance:.2f}")
+
+# Main function to run the cryptocurrency wallet program.
+def main():
+    initial_balance = float(input("Starting balance: "))  # Get the initial balance from the user.
+    wallet = CryptoWallet(initial_balance)  # Create a wallet object with the initial balance.
+
+    # Display the main menu and handle user choices.
+    while True:
+        print("\nCrypto Wallet Menu:")
+        print("1. Send Crypto")
+        print("2. Receive Crypto")
+        print("3. Check Balance")
+        print("4. Exit")
+
+        choice = input("Select an option: ")  # Get the user's menu choice.
+
+        # Handle the user's choice based on the selected option.
+        if choice == "1":
+            amount = float(input("Amount to send: "))  # Get the amount to send from the user.
+            wallet.send(amount)  # Call the send method of the wallet.
+        elif choice == "2":
+            amount = float(input("Amount to receive: "))  # Get the amount to receive from the user.
+            wallet.receive(amount)  # Call the receive method of the wallet.
+        elif choice == "3":
+            print(f"Balance: {wallet.balance:.2f}")  # Display the current wallet balance.
+        elif choice == "4":
+            print("Exiting.")  # Exit the program loop.
+            break
+        else:
+            print("Invalid option. Try again.")  # Handle invalid menu choices.
+
+# Entry point of the program: execute the main function if the script is run directly.
+if __name__ == "__main__":
+    main()
